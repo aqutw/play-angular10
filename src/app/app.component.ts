4dc0ee1b3562f40060388e5194866f104825251b
aqutw <aquarian.EX@hotmail.com>
2020-08-08 23:57:00 +0800

    ng g c userList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'youtube';
+  str1 = 'string111';
+  obj = {
+    a: 1,
+    b: 2
+  };
+  arr = [
+    'a1',
+    'b2'];
+  siteUrl = window.location.href;
+  getName(): string{
+    return 'peter';
+  }
 }
