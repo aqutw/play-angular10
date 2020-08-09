e1a7b8560149f800c677aaca171825d6ac66a029
aqutw <aquarian.EX@hotmail.com>
2020-08-09 14:56:44 +0800

    ng g c users (Make users component)

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { UsersComponent } from './users.component';
 
 
 
 @NgModule({
-  declarations: [LoginComponent, SignupComponent],
+  declarations: [LoginComponent, SignupComponent, UsersComponent],
   imports: [
     CommonModule
   ],
