53688e0ab80c57a71bed17199579dd880b116ac6
aqutw <aquarian.EX@hotmail.com>
2020-08-09 13:53:00 +0800

    ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isShow = true;
+  showFlag = 'yes';
+  color = 'green';
   title = 'youtube';
   str1 = 'string111';
   linkHref = '';
